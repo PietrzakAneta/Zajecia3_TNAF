4a83fc204f78ddeee8a767e73a04525b5124c1c3
PietrzakAneta <anetapietrzak0@gmail.com>
2022-11-29 09:08:25 +0100

    Proste funkcje

---
+++ zad1/functions.js
@@ -0,0 +1,20 @@
+function isOdd(x){
+    if(x%2 ==1)
+    return true
+    else 
+    return false
+}
+
+function isOdd2(x){
+    return (x%2 ==1)
+}
+
+
+const isEven = x  => {
+    return (x%2 == 0)
+}
+
+
+console.log(isOdd(10));
+console.log(isOdd(11));
+console.log(isEven(12));
